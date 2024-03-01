c4d26818fac674eb4f0c386dc9e4e831824e3146
pedroC8 <pedroguitarvox@outlook.com>
2024-03-01 13:24:22 -0300

    faltou a dois

---
+++ lista 2/exercicio 1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+     public static void Main (string[] args)
+      {
+         Console.WriteLine(" digite seu usuario: " );
+         string admin = Console.ReadLine();
+
+         Console.WriteLine("digite sua senha ");
+         string Senhasalva = Console.ReadLine();
+
+     
+         if (Senhasalva == "123senha")
+        {
+          
+             Console.WriteLine("senha esta correta ");
+        } 
+         else
+         {
+
+           Console.WriteLine("credenciais incorretas ");
+         }
+
+
+    
+
+
+     }
+}
+
+   
+    
---
+++ lista 2/exercicio 3/Program.cs
@@ -0,0 +1,24 @@
+﻿using  System; 
+
+class Program
+{
+    public static void Main(string{} args) 
+    {
+      Console.WriteLine("qual filho(a):");
+      string nomedofilhodamulher = Console.ReadLine();
+    
+
+      if (nomedofilhodamulher == "joselito" ) 
+      {
+        
+         Console.WriteLine("1.escola das 7h as 12h 2. almoco das 13 as 14 3. natacao das 14h as 16h 4. reforco escolar das 16h as 19h");
+      
+      }
+      else if (nomedofilhodamulher == "filomena") 
+      {
+      
+        Console.WriteLine("1. escola das 7h as 12h 2. almoco das 12h as 13h 3. futebol das 14h as 16h 4. dever de casa das 16h as 18h");
+      
+      }
+    }
+}
